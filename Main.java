1c5fd6d6c1e984cbd5ba69e3ea634fa200d2613f
Micheal Bailey <mbaileyfuturist@gmail.com>
2020-09-27 14:04:30 -0400

    Prompt the user for their car information and use input as fields for ParkedCar object.

--- Main.java
+++ Main.java
@@ -1,11 +1,35 @@
 package Project3;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		String make, model, color, liscenseNumber; 
+		int minutesParked;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Please enter the car make: ");
+		make = sc.nextLine();
+		
+		System.out.print("Please enter the car model: ");
+		model = sc.nextLine();
+		
+		System.out.print("Please enter the car color: ");
+		color = sc.nextLine();
+		
+		System.out.print("Please enter the car liscense number: ");
+		liscenseNumber = sc.nextLine();
+		
+		System.out.print("Please enter the time parked in minutes: ");
+		minutesParked = sc.nextInt();
+
+
+		
 		//ParkedCar Object: Ask for the make, model, color, license number and minutes parked.
-		ParkedCar parkedCar = new ParkedCar("Ford", "Mustang", "Green", "123456", 180);
+		ParkedCar parkedCar = new ParkedCar(make, model, color, liscenseNumber, minutesParked);
 		
 		//ParkingMeter Object: Ask for the time purchased in minutes.
 		ParkingMeter parkingMeter = new ParkingMeter(40);
